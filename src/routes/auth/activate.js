4091f604d42f999de63b284a43f4c05bcdf54dcd
Boban Acimovic <boban.acimovic@gmail.com>
2020-04-13 17:20:46 +0200

    improve activation

--- src/routes/auth/activate.js
+++ src/routes/auth/activate.js
@@ -1,14 +1,20 @@
 import User from "../../models/User";
+import jwt from "jsonwebtoken";
 
 export async function get(req, res, next) {
-  const decoded = jwt.verify(
-    req.params.query.token,
-    process.env.JWT_SECRET_KEY
-  );
-  const user = await User.findOne({ username: decoded.username });
-
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).end({
-    user,
-  });
+  try {
+    const decoded = jwt.verify(
+      req.params.query.token,
+      process.env.JWT_SECRET_KEY
+    );
+    const user = await User.findOne({ username: decoded.username });
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).end({
+      user,
+    });
+  } catch (err) {
+    res.status(500).end({
+      error: err.message,
+    });
+  }
 }
