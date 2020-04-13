38b80be9eb5d55dac3a38c463702faf6db0df49b
Boban Acimovic <boban.acimovic@gmail.com>
2020-04-13 17:16:36 +0200

    use async

--- src/routes/auth/activate.js
+++ src/routes/auth/activate.js
@@ -1,8 +1,11 @@
 import User from "../../models/User";
 
-export function get(req, res, next) {
-  const decoded = jwt.verify(req.params.query.token, process.env.JWT_SECRET_KEY);
-  const user = await User.findOne({username: decoded.username});
+export async function get(req, res, next) {
+  const decoded = jwt.verify(
+    req.params.query.token,
+    process.env.JWT_SECRET_KEY
+  );
+  const user = await User.findOne({ username: decoded.username });
 
   res.setHeader("Content-Type", "application/json");
   res.status(200).end({
