b03d9b5c09dc7fd36bb9480401c7363ada65924a
Boban Acimovic <boban.acimovic@gmail.com>
2020-08-08 21:38:24 +0200

    update sapper

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import commonjs from "@rollup/plugin-commonjs";
 import svelte from "rollup-plugin-svelte";
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import config from "sapper/config/rollup.js";
 import pkg from "./package.json";
@@ -14,6 +14,7 @@ const dev = mode === "development";
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
+  (warning.code === "MISSING_EXPORT" && /'preload'/.test(warning.message)) ||
   (warning.code === "CIRCULAR_DEPENDENCY" &&
     /[/\\]@sapper[/\\]/.test(warning.message)) ||
   onwarn(warning);
@@ -40,35 +41,36 @@ export default {
       commonjs(),
 
       legacy &&
-      babel({
-        extensions: [".js", ".mjs", ".html", ".svelte"],
-        runtimeHelpers: true,
-        exclude: ["node_modules/@babel/**"],
-        presets: [
-          [
-            "@babel/preset-env",
-            {
-              targets: "> 0.25%, not dead",
-            },
+        babel({
+          extensions: [".js", ".mjs", ".html", ".svelte"],
+          babelHelpers: "runtime",
+          exclude: ["node_modules/@babel/**"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: "> 0.25%, not dead",
+              },
+            ],
           ],
-        ],
-        plugins: [
-          "@babel/plugin-syntax-dynamic-import",
-          [
-            "@babel/plugin-transform-runtime",
-            {
-              useESModules: true,
-            },
+          plugins: [
+            "@babel/plugin-syntax-dynamic-import",
+            [
+              "@babel/plugin-transform-runtime",
+              {
+                useESModules: true,
+              },
+            ],
           ],
-        ],
-      }),
+        }),
 
       !dev &&
-      terser({
-        module: true,
-      }),
+        terser({
+          module: true,
+        }),
     ],
-    preserveEntrySignatures: 'strict',
+
+    preserveEntrySignatures: false,
     onwarn,
   },
 
@@ -82,6 +84,7 @@ export default {
       }),
       svelte({
         generate: "ssr",
+        hydratable: true,
         dev,
       }),
       resolve({
@@ -95,9 +98,10 @@ export default {
       }),
     ],
     external: Object.keys(pkg.dependencies).concat(
-      require("module").builtinModules ||
-      Object.keys(process.binding("natives"))
+      require("module").builtinModules
     ),
+
+    preserveEntrySignatures: "strict",
     onwarn,
   },
 
@@ -116,6 +120,7 @@ export default {
       !dev && terser(),
     ],
 
+    preserveEntrySignatures: false,
     onwarn,
   },
 };
